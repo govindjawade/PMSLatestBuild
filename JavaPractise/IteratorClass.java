8f83997e7732237fba79c6b99d7db67dde8c4a68
Admin <govindjawade19@gmail.com>
2024-09-25 04:34:08 +0530

    pp

---
+++ JavaPractise/IteratorClass.java
@@ -0,0 +1,17 @@
+package JavaPractise;
+
+import java.util.ArrayList;
+
+public class IteratorClass {
+
+    public static void main(String[] args) {
+        ArrayList a1 = new ArrayList();
+        a1.add("govind");//boolean function
+        a1.add("kisan");
+        a1.add("jawade");
+        a1.add("priti");
+        a1.add("priti");
+        a1.add("shrisha");
+        System.out.println(a1);
+    }
+}
